35a35d48a0324268e7a50dcc166e8f232a28c9ad
Advaith Doosa <advaithseo@gmail.com>
2018-09-12 18:19:40 -0700

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  parser: "babel-eslint",
+  extends: ["airbnb", "prettier"],
+  plugins: ["react", "jsx-a11y", "import"]
+}
+module.exports = {
+  parser: "babel-eslint",
+  extends: ["airbnb", "prettier"],
+  plugins: ["react", "jsx-a11y", "import"]
+};
